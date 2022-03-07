6a66dce8734f7f8dc9420ac9234f6c06bdedf418
Elasticnoise <vs.tretyakov01@gmail.com>
2022-03-07 19:38:40 +0300

    add ex00

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -1,2 +1,21 @@
+#include <iostream>
 
+int	main(int argc, char **argv)
+{
+	int i = 1;
+	int j;
 
+	if (argc > 1) {
+		while (argv[i]) {
+			j = 0;
+			while (argv[i][j]) {
+				std::cout << (char)std::toupper(argv[i][j]);
+				j++;
+			}
+			i++;
+		}
+		std::cout << std::endl;
+	} else {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+}
