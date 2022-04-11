57437bc7ae5676b31b6404899b8324228b0e698f
Elasticnoise <vs.tretyakov01@gmail.com>
2022-04-11 22:41:22 +0300

    module_05: ex03(developing)

--- module_05/ex03/Intern.cpp
+++ module_05/ex03/Intern.cpp
@@ -1,26 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Intern.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lechalme <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/08 21:25:51 by lechalme          #+#    #+#             */
-/*   Updated: 2022/04/08 21:25:53 by lechalme         ###   ########.fr       */
+/*   Created: 2022/04/11 22:38:36 by lechalme          #+#    #+#             */
+/*   Updated: 2022/04/11 22:38:40 by lechalme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Bureaucrat.hpp"
-#include "Form.hpp"
-#include "PresidentialPardonForm.hpp"
-#include "ShrubberyCreationForm.hpp"
-#include "RobotomyRequestForm.hpp"
-#include "Intern.hpp"
 
-int main()
-{
-//	Intern someRandomIntern;
-//	Form* rrf;
-//	rrf = someRandomIntern.makeForm("robotomy request", "Bender");
-	return 0;
-}
--- module_05/ex03/Intern.hpp
+++ module_05/ex03/Intern.hpp
@@ -1,26 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Intern.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lechalme <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/08 21:25:51 by lechalme          #+#    #+#             */
-/*   Updated: 2022/04/08 21:25:53 by lechalme         ###   ########.fr       */
+/*   Created: 2022/04/11 22:38:44 by lechalme          #+#    #+#             */
+/*   Updated: 2022/04/11 22:38:46 by lechalme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Bureaucrat.hpp"
-#include "Form.hpp"
-#include "PresidentialPardonForm.hpp"
-#include "ShrubberyCreationForm.hpp"
-#include "RobotomyRequestForm.hpp"
-#include "Intern.hpp"
+#ifndef INTERN_HPP
+#define INTERN_HPP
 
-int main()
+#include "Form.hpp"
+
+class Intern
 {
-//	Intern someRandomIntern;
-//	Form* rrf;
-//	rrf = someRandomIntern.makeForm("robotomy request", "Bender");
-	return 0;
-}
+public:
+	Intern();
+	Intern(const Intern& other);
+	~Intern();
+
+#endif
+
