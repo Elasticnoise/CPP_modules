6631c1867c9b34b548f7e9c8214ddf11a0686941
Elasticnoise <vs.tretyakov01@gmail.com>
2022-04-22 21:33:13 +0300

    module_08: ex02(init)

---
+++ module_08/ex02/MutantStack.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lechalme <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/22 21:05:24 by lechalme          #+#    #+#             */
+/*   Updated: 2022/04/22 21:05:25 by lechalme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+#include <deque>
+
+template<typename T, typename Container = std::deque<T>>
+class MutantStack: public std::stack<T, Container>
+{
+public:
+	MutantStack() {};
+	MutantStack(const MutantStack<T, Container>& other) {*this = other};
+	MutantStack &operator =(const MutantStack<T, Container >& other)
+	{
+		*this = other;
+		return *this;
+	}
+	~MutantStack();
+};
+
+#endif
